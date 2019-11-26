81ea6708074fbe638ef3c162bdd0397b4f64ba82
mathislrn <mathis.lrn061198@gmail.com>
2019-11-26 16:29:33 +0100

    feat(app): adding login window

---
+++ crud-app/src/com/crudapp/tools/AppInfo.java
@@ -0,0 +1,9 @@
+package com.crudapp.tools;
+
+public class AppInfo {
+    public static final String APP_NAME = "CRUD App";
+    public static final int APP_VERSION_CODE = 1;
+    public static final String APP_VERSION = "1.0";
+    public static final String APP_PRINT_NAME = String.format("%s (v%s)", APP_NAME, APP_VERSION);
+    public static final String APP_LOGIN_NAME = String.format("%s - %s", APP_NAME, "Login");
+}
