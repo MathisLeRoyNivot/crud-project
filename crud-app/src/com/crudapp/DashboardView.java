80096364ab1587df03535f39f8974513d765403b
mathislrn <mathis.lrn061198@gmail.com>
2019-12-17 14:53:41 +0100

    Adding customer react app project folder

---
+++ crud-app/src/com/crudapp/DashboardView.java
@@ -0,0 +1,15 @@
+package com.crudapp;
+
+import com.crudapp.tools.AppInfo;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ */
+public class DashboardView {
+
+}
