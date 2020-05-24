61f3a611ca1041ae2bdef4d1f00cc99b9471c5a0
adrienvaucard <adrien.vaucard@gmail.com>
2020-05-24 15:37:54 +0200

    feat(BE FRONT): Working GET and DELETE on tables

--- web-front/src/app/pages/columns/columns.page.ts
+++ web-front/src/app/pages/columns/columns.page.ts
@@ -30,4 +30,16 @@ export class ColumnsPage implements OnInit {
     );
   }
 
+  deleteRow(id: number) {
+    console.log("zizi");
+    this.http.delete('http://localhost:3000/' + this.table + '/' + id).subscribe(
+			(data: any) => {
+        window.location.reload();
+      },
+			(error) => {
+				console.log(error);
+			}
+    );
+  }
+
 }
